1581ed80fd53f53744be29d7a4d079ce1f616f5f
Yacoo-Song <songleson@163.com>
2025-12-31 15:44:08 +0800

    build: 生成了构建输出的 index.html 文件并更新了 Vite 配置

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './', // 关键配置：使用相对路径，适配 GitHub Pages
+  base: '/price-ninja/', // 关键配置：使用相对路径，适配 GitHub Pages
   server: {
     port: 3000,
     open: true
